800b891a5f387d9db1acbeb5b55937ae91b3a09b
Vitor Hirt <vitordev.webdeveloper@outlook,.com>
2025-08-09 02:03:12 -0300

    Novas funcionalidades

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,12 @@ use App\Http\Controllers\NFSController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\ViewController;
 use App\Http\Middleware\NFSMiddleware;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MainController::class, 'index'])->name('index');
+//Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
 Route::get('/test/{value}', [MainController::class, 'upperCase'])->name('test');
 
@@ -56,7 +57,11 @@ Route::get('/products', function (): View {
 
 // middleware NFS
 
-Route::middleware([NFSMiddleware::class])->prefix('admin')->group(function(){
+Route::middleware([NFSMiddleware::class])->prefix('admin')->group(function () {
     Route::get('/nfs', [NFSController::class, 'index'])->name('admin.nfs');
     Route::get('/nfs/search', [NFSController::class, 'infoNFS'])->name('admin.nfs.id');
 });
+
+// Route viewShow
+
+Route::get('/', [ViewController::class, 'showView']);
